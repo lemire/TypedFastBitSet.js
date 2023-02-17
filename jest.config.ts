b99bd22696270eda131049b170541cb3df525d3f
Alex Miller <codex.nz@gmail.com>
2023-02-17 15:06:30 +1300

    Use updated ts-jest config api

--- jest.config.ts
+++ jest.config.ts
@@ -1,21 +1,18 @@
 import type { Config } from "@jest/types";
 import { join } from "path";
-import type { TsJestGlobalOptions } from "ts-jest";
-
-const tsJestCfg: TsJestGlobalOptions = {
-  tsconfig: join(__dirname, "tsconfig.json"),
-};
 
 const config: Config.InitialOptions = {
-  globals: {
-    "ts-jest": tsJestCfg,
-  },
   moduleFileExtensions: ["ts", "js", "json", "node"],
   roots: ["<rootDir>/src"],
   testEnvironment: "node",
   testRegex: "(\\.|/)(test|spec)\\.ts$",
   transform: {
-    "^.+\\.ts$": "ts-jest",
+    "^.+\\.ts$": [
+      "ts-jest",
+      {
+        tsconfig: join(__dirname, "tsconfig.json"),
+      },
+    ],
   },
 };
 
